94a1064ebf1361ed00c9879f879f89c74339df84
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-01-05 10:39:53 +0100

    Javascript les modules

---
+++ les_modules/main2.js
@@ -0,0 +1,40 @@
+
+
+//pour l'export exemple default
+
+import maFonction from './baseFuncs.js';
+
+maFonction(15, 5);
+
+
+
+
+//Pour tous importer Exemple 3
+
+// import * as baseFuncs from './baseFuncs.js';
+
+// baseFuncs.soustraction(10,5);
+
+// console.log(baseFuncs.nom);
+
+
+
+
+
+//une 2éme manière d'importer Exemple 2
+// import {soustraction, nom as empereur} from "./baseFuncs.js";
+// soustraction(10,5);
+
+// console.log(empereur);
+
+
+
+
+
+
+//Une manière d'importer Exemple 1
+// import { soustraction, nom } from "./baseFuncs.js";
+
+// soustraction(10,5);
+
+// console.log(nom);
